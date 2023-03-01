8957018e6e7a02f3f39d7bd9a1641dc401d4473b
lutrx <lisa.bieker@web.de>
2023-03-01 22:30:08 +0100

    finalized lab 2.05

--- solutions_2.05.sql
+++ solutions_2.05.sql
@@ -5,7 +5,8 @@ SELECT * FROM sakila.actor
 WHERE first_name LIKE '%Scarlett%';
 
 -- 2. How many films (movies) are available for rent and how many films have been rented?
--- ?
+SELECT COUNT(DISTINCT inventory_id) AS 'available for rent', COUNT(*) AS 'have been rented'
+FROM sakila.rental;
 
 -- 3. What are the shortest and longest movie duration? Name the values max_duration and min_duration.
 SELECT MIN(length) AS 'min_duration', MAX(length) AS 'max_duration'
@@ -21,10 +22,34 @@ SELECT COUNT(DISTINCT last_name) FROM sakila.actor;
 SELECT MAX(DATEDIFF(last_update, rental_date)) FROM sakila.rental;
 
 -- 7. Show rental info with additional columns month and weekday. Get 20 results.
-SELECT * FROM sakila.rental;
-
-SELECT *, date_format(rental_date, '%M') AS 'Rental Month', date_format(rental_date, '%D') AS 'Rental Day', date_format(return_date, '%M') AS 'Return Month', date_format(return_date, '%D') AS 'Return Day'
+SELECT *, date_format(rental_date, '%M') AS 'Rental Month', DAYNAME(rental_date) AS 'Rental Day', date_format(return_date, '%M') AS 'Return Month', DAYNAME(return_date) AS 'Return Day'
 FROM sakila.rental
 LIMIT 20;
 
 -- 8. Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
+SELECT *,
+CASE
+WHEN WEEKDAY(rental_date) IN (0, 1, 2, 3, 4) then 'workday'
+ELSE 'weekend'
+END AS 'day_type'
+FROM sakila.rental;
+
+-- 9. Get release years
+SELECT DISTINCT release_year FROM sakila.film;
+
+-- 10. Get all films with ARMAGEDDON in the title.
+SELECT title FROM sakila.film
+WHERE title LIKE '%ARMAGEDDON%';
+
+-- 11. Get all films which title ends with APOLLO.
+SELECT title FROM sakila.film
+WHERE title LIKE '%APOLLO';
+
+-- 12. Get 10 the longest films.
+SELECT length, title FROM sakila.film
+ORDER BY length DESC
+LIMIT 10;
+
+-- 13. How many films include Behind the Scenes content?
+SELECT COUNT(special_features) FROM sakila.film
+WHERE special_features LIKE '%Behind the Scenes%';
